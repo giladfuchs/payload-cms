f4aea63df67929583a3cb506f30da1be983527c7
giladfuchs <igilfu@gmail.com>
2026-07-21 13:47:06 +0300

    update

--- src/lib/core/types/payload-types.ts
+++ src/lib/core/types/payload-types.ts
@@ -282,6 +282,7 @@ export interface Post {
 export interface Media {
   id: number;
   alt: string;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1194,6 +1195,7 @@ export interface PostsSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
